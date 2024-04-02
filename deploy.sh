4a6e3b393dd1ed9bbe64ee53ff796e70fb922bac
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-02 14:39:16 -0400

    feature: add cronjob through pm2

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ source .env
 
 # Compiles bot binary
 bun build ./src/index.ts --compile --outfile leetcode-bot  
+bun build ./src/cronjob.ts --compile --outfile update-problem-index
 
 # Run bot as daemon through `pm2` utility
 pm2 start --name "zen-leetcode-ds-bot" leetcode-bot
+pm2 start --name "update-leetcode-problem-index" update-problem-index --cron "*/1 * * * *"
